92f72b8af57e615aa39505e56b956ae411d84ab9
Sai-Kiran-Reddy-Muppa <saikiranreddy1652@gmail.com>
2026-01-12 14:12:55 +0530

    Add axios and fix general.js with async await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,45 +1,60 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
-let books = require('../books.json');
+
+const BOOKS_URL = 'http://localhost:5000/';
 
 // Get all books
-router.get('/', (req, res) => {
-  res.json(books);
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching books" });
+  }
 });
 
 // Get book by ISBN
-router.get('/isbn/:isbn', (req, res) => {
-  const isbn = req.params.isbn;
-  res.json(books[isbn]);
+router.get('/isbn/:isbn', async (req, res) => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    const book = response.data[req.params.isbn];
+    book ? res.json(book) : res.status(404).json({ message: "Book not found" });
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
 });
 
 // Get books by author
-router.get('/author/:author', (req, res) => {
-  const author = req.params.author;
-  let result = {};
-  Object.keys(books).forEach(isbn => {
-    if (books[isbn].author === author) {
-      result[isbn] = books[isbn];
-    }
-  });
-  res.json(result);
+router.get('/author/:author', async (req, res) => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    const books = response.data;
+    const result = Object.fromEntries(
+      Object.entries(books).filter(
+        ([, book]) => book.author === req.params.author
+      )
+    );
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
 });
 
 // Get books by title
-router.get('/title/:title', (req, res) => {
-  const title = req.params.title;
-  let result = {};
-  Object.keys(books).forEach(isbn => {
-    if (books[isbn].title === title) {
-      result[isbn] = books[isbn];
-    }
-  });
-  res.json(result);
+router.get('/title/:title', async (req, res) => {
+  try {
+    const response = await axios.get(BOOKS_URL);
+    const books = response.data;
+    const result = Object.fromEntries(
+      Object.entries(books).filter(
+        ([, book]) => book.title === req.params.title
+      )
+    );
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Error" });
+  }
 });
 
-// Get book reviews
-router.get('/review/:isbn', (req, res) => {
-  const isbn = req.params.isbn;
-  res.json(books[isbn].reviews);
-});
 module.exports.general = router;
